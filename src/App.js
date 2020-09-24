50ec94986bf0d4eafad9de6c6c2d7e4c9f681ef5
Andrei Bardas <bardas.andrei17@gmail.com>
2020-09-24 10:10:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Navbar from './Components/Header/navbar.component';
+import Post from './Components/Post/post.component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <div className="posts">
+      <Post title="ceva" description="altceva"/>
+      <Post title="post 2" description="content"></Post>
+      </div>
     </div>
   );
 }
